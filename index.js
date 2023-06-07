28a20b7195590748aa88dd23e9c3802de05d331d
Hallie Sala <halliesala19@gmail.com>
2023-06-07 13:04:02 -0400

    complete objects lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {'name': 'Joe Biden', 'streetAddress': 'White House'};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const shallowCopy = {...employee};
+    shallowCopy[key] = value;
+    return shallowCopy;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const shallowCopy = {...employee};
+    delete shallowCopy[key];
+    return shallowCopy;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
